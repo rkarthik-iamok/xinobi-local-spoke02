1682f1df46c9a37e1fe2b69d4b6a1991cf13be3e
rkarthik-iamok <rkarthik.iamok@gmail.com>
2024-06-24 18:35:44 -0400

    Added Signup button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@
 
 import { useOktaAuth } from "@okta/okta-react";
 import React, { useState, useEffect } from "react";
-import { redirect } from "react-router-dom";
+
 import { Button, Header } from "semantic-ui-react";
 
 const Home = () => {
@@ -38,7 +38,8 @@ const Home = () => {
   };
 
   const signup = async () => {
-    redirect("https://");
+    window.location.href =
+      "https://ciam-spoke02.karthiktc.com/oauth2/default/v1/authorize?client_id=0oac2qpybeHSxXr7T697&code_challenge=AptwK4s28Vrla9GDeSLhE9QEDT_p41ZihtfDKgeKU2A&code_challenge_method=S256&nonce=yuxWTTyxdoAd2RuPGxsPIkrevxnrYlhKjGq3Fdr34rgepSuCHRGiOhlPARa0H78K&redirect_uri=https%3A%2F%2Flocal-app-01.karthiktc.com%2Flogin%2Fcallback&response_type=code&state=qOvqq0XLGbIVpk1IZRoCkNC8JnxxBzzE02ZAd1FvnpkwGN5uQcWqYvUyL6UolXSK&acr_values=urn%3Aokta%3Aloa%3A1fa%3Apwd&scope=openid%20email%20profile%20offline_access&signup_page=true";
   };
 
   if (!authState) {
